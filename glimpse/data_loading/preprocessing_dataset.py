41d722effc76fd31d22dd4276e86cf8e3cb6e777
Stefano Brilli <8728861+Marlowess@users.noreply.github.com>
2025-01-28 23:48:42 +0100

    [MULTILANG] Implement the `split_database` function
    In this commit, we implement a function to split one or more
    dataframes into train, val and test datasets

---
+++ glimpse/data_loading/preprocessing_dataset.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+
+def split_dataset(base_folder_path, input_file_path_list, output_folder_path, random_state=42):
+    """
+    This function reads the input files and splits the dataset into train, test and validation datasets.
+    The datasets are saved in the preprocessed folder.
+    """
+    # Read all the inputs files and concatenate in a pandas dataframe
+    x_label, y_label = 'review', 'metareview'
+    df = pd.concat([pd.read_csv(f"{base_folder_path}/{file_path}") for file_path in input_file_path_list], ignore_index=True)
+
+    # Split the dataset into train, test and validation, keeping all the columns in the final datasets
+    x_train, x_test, y_train, y_test = train_test_split(df[x_label], df[y_label], test_size=0.2, random_state=random_state)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=random_state)
+
+    # Create a complete dataframe for each dataset
+    train_df = pd.DataFrame({'id': 0, x_label: x_train, y_label: y_train})
+    test_df = pd.DataFrame({'id': 0, x_label: x_test, y_label: y_test})
+    val_df = pd.DataFrame({'id': 0, x_label: x_val, y_label: y_val})
+
+    # Save the datasets in the processed folder
+    # Check if the output folder exists, otherwise create it
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+    train_df.to_csv(f'{output_folder_path}/train.csv', index=False)
+    test_df.to_csv(f'{output_folder_path}/test.csv', index=False)
+    val_df.to_csv(f'{output_folder_path}/val.csv', index=False)
