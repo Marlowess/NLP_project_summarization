8a1c70968937dad749a0b400c919bbfcb8c9b0fe
stefano_gamba <s.gamba@reply.it>
2025-02-08 17:55:23 +0100

    feat(visualize_results): draw plots to visualize results

---
+++ glimpse/evaluate/evaluate_llm_as_expert/visualize_results.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+import argparse
+from pathlib import Path
+import os
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--evaluation_dataset", type=Path, default="", required=True)
+    parser.add_argument("--type", type=str, default="", required=True)
+    parser.add_argument("--model", type=str, default="", required=True)
+    parser.add_argument("--model_b", type=str, default="")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    folder_path = "plots"
+    os.makedirs(folder_path, exist_ok=True)
+
+    evaluation_type = args.type
+    evaluation_df = pd.read_csv(args.evaluation_dataset)
+    if evaluation_type == 'pairwise':
+        cols = ['common_ideas', 'unique_ideas', 'best_overall']
+        for col in cols:
+            evaluation_df.groupby(col).size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
+            plt.gca().spines[['top', 'right',]].set_visible(False)
+            filepath = os.path.join(folder_path, f"plot_{col}_{args.model}_vs_{args.model_b}.pdf")
+            plt.savefig(filepath, format="pdf", dpi=300)
+            plt.close()
+    elif evaluation_type == 'score':
+        cols = []
+        # todo
+
+
+
+if __name__ == "__main__":
+    main()
